678ed067c9c85084fa689e8f9974b257b00edb80
Mark Skelton <markskelton10@gmail.com>
2024-08-29 13:33:10 +0100

    Update to_s() method in Board class
    I want the board to be printed in a prettier way, showing the border
    lines in each cell rather than a 2D array. This makes for a better user
    experience.
    
    I updated the initial board to be full of ' ' instead of nil values for
    better presentation of empty cells.
    
    I broke up the to_s() method using the private row() helper method to
    improve readability.

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,16 @@
 # It has methods to detect if full, if winner, to print, and to update the board.
 class Board
   def initialize
-    @board = Array.new(3) { Array.new(3) }
+    @board = Array.new(3) { Array.new(3, ' ') }
+  end
+
+  def to_s
+    "#{row(0)}\n-----------\n#{row(1)}\n-----------\n#{row(2)}"
+  end
+
+  private
+
+  def row(row_num)
+    " #{@board[row_num][0]} | #{@board[row_num][1]} | #{@board[row_num][2]} "
   end
 end
---
+++ main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'lib/board'
+require_relative 'lib/game'
+
+board = Board.new
+puts board
