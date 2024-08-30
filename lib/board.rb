8d627235468757cc88fc901303384e4f5ef939d5
Mark Skelton <markskelton10@gmail.com>
2024-08-30 11:46:34 +0100

    Move end game logic to Game class
    Checking for a winner or if it is a draw is the job of the Game class.
    
    Untested since I will have to figure out the interface a bit more and
    refactor.

--- lib/board.rb
+++ lib/board.rb
@@ -11,15 +11,6 @@ class Board
     @board = Array.new(BOARD_LENGTH) { Array.new(BOARD_LENGTH, ' ') }
   end
 
-  def winner?
-    # Check rows, check cols, check diagonals
-    win_rows?(@board) || win_rows?(@board.transpose) || win_diags?
-  end
-
-  def full?
-    @board.flatten.none?(' ')
-  end
-
   def to_s
     "#{row(0)}\n-----------\n#{row(1)}\n-----------\n#{row(2)}"
   end
@@ -28,19 +19,5 @@ class Board
 
   def row(row_num)
     " #{@board.dig(row_num, 0)} | #{@board.dig(row_num, 1)} | #{@board.dig(row_num, 2)} "
-  end
-
-  def win_rows?(board)
-    board.each { |row| return true if row.all?('X') || row.all?('O') }
-    false
-  end
-
-  def win_diags?
-    win_diag?(0, 2) || win_diag?(2, 0)
-  end
-
-  def win_diag?(start_col, end_col)
-    diag = [@board.dig(0, start_col), @board.dig(1, 1), @board.dig(2, end_col)]
-    diag.all?('X') || diag.all?('O')
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,31 @@ class Game
   def play_turn
     @players[@player_turn].place_token(@board)
     puts @board
-    # Check end game
     @player_turn = (@player_turn + 1) % 2
+  end
+
+  def winner?
+    # Check rows, check cols, check diagonals
+    win_rows?(@board.board) || win_rows?(@board.board.transpose) || win_diags?
+  end
+
+  def full?
+    @board.board.flatten.none?(' ')
+  end
+
+  private
+
+  def win_rows?(board)
+    board.each { |row| return true if row.all?('X') || row.all?('O') }
+    false
+  end
+
+  def win_diags?
+    win_diag?(0, 2) || win_diag?(2, 0)
+  end
+
+  def win_diag?(start_col, end_col)
+    diag = [@board.board.dig(0, start_col), @board.board.dig(1, 1), @board.board.dig(2, end_col)]
+    diag.all?('X') || diag.all?('O')
   end
 end
