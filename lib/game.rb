e572a52dff5d6af4e041ea1b35f20c78f1ef862a
Mark Skelton <markskelton10@gmail.com>
2024-08-30 15:08:17 +0100

    Move loop from main file to Game class
    This loop was logic for playing the game so it belongs in the Game
    class.
    
    Moved lines from play to play_turn method, to reduce play method size.
    It also makes sense now that play_turn gets the current player to move,
    checks for game over, switches the player turn, and prints the board;
    this all counts as a turn I think.
    
    Created current_player local variable and switch_player_turn method for
    easier readability.
    
    The check_game_over method now checks for game over before
    switch_player_turn which allows us to check on the current player and
    not look back on the previous player after switching. This makes the
    logic easier to follow.
    
    The switch_player_turn method uses a different way to switch,
    (1 - index) with index either 0 or 1. This is easier to understand than
    adding 1 modulo 2.

--- lib/game.rb
+++ lib/game.rb
@@ -6,8 +6,6 @@ require_relative 'player'
 # Game holds info about the game state: board state, players, player turn, and result.
 # It has methods for playing a turn and checking for game over
 class Game
-  attr_reader :board, :result
-
   def initialize
     @board = Board.new
     @players = [Player.new('X'), Player.new('O')]
@@ -15,16 +13,30 @@ class Game
     @result = nil
   end
 
-  def play_turn
-    @players[@player_turn].place_token(@board.board)
-    @player_turn = (@player_turn + 1) % 2
+  def play
+    puts @board
+    play_turn while @result.nil?
+    puts @result
   end
 
-  def check_game_over
+  private
+
+  def play_turn
+    current_player = @players[@player_turn]
+    current_player.place_token(@board.board)
+    check_game_over(current_player)
+    switch_player_turn
+    puts @board
+  end
+
+  def check_game_over(current_player)
     # Only the player that has just moved can win the game
-    player_just_moved = @players[@player_turn - 1]
-    return @result = "Player #{player_just_moved.token} has won the game!" if player_just_moved.winner?(@board.board)
+    return @result = "Player #{current_player.token} has won the game!" if current_player.winner?(@board.board)
 
     @result = 'Draw!' if @board.full?
+  end
+
+  def switch_player_turn
+    @player_turn = 1 - @player_turn
   end
 end
--- main.rb
+++ main.rb
@@ -2,11 +2,4 @@
 
 require_relative 'lib/game'
 
-game = Game.new
-puts game.board
-while game.result.nil?
-  game.play_turn
-  puts game.board
-  game.check_game_over
-end
-puts game.result
+Game.new.play
