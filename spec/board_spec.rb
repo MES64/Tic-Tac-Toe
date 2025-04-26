2ce860ae4a1fc42abbc5ab07d28c5ea8f25766d7
Mark Skelton <markskelton10@gmail.com>
2025-04-26 07:22:27 +0100

    test: create tests for place_token of Board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,9 +5,11 @@ require_relative '../lib/board'
 RSpec.describe Board do
   describe '#at' do
     # Incoming Query Message -> Test return value
-    board = [['X', ' ', 'O'],
-             [' ', ' ', 'X'],
-             ['O', 'X', 'O']]
+    let(:board) do
+      [['X', ' ', 'O'],
+       [' ', ' ', 'X'],
+       ['O', 'X', 'O']]
+    end
     subject(:board_at) { described_class.new(board) }
 
     context 'when the chosen coordinates are on the board' do
@@ -46,6 +48,117 @@ RSpec.describe Board do
       it 'returns nil for coordinates [1, 3]' do
         token = board_at.at([1, 3])
         expect(token).to be_nil
+      end
+    end
+  end
+
+  describe '#place_token' do
+    # Incoming Command Message -> Test that board is updated correctly
+    context 'when place O token over X token at [0, 0]' do
+      let(:board) do
+        [['X', ' ', 'O'],
+         [' ', ' ', 'X'],
+         ['O', 'X', 'O']]
+      end
+      subject(:board_place_O) { described_class.new(board) }
+
+      it 'updates X token with O token at [0, 0] only' do
+        expected_board = [['O', ' ', 'O'],
+                          [' ', ' ', 'X'],
+                          ['O', 'X', 'O']]
+        board_place_O.place_token('O', [0, 0])
+        actual_board = board_place_O.board
+        expect(actual_board).to eql(expected_board)
+      end
+    end
+
+    context 'when place X token over empty space at [1, 1]' do
+      let(:board) do
+        [['X', ' ', 'O'],
+         [' ', ' ', 'X'],
+         ['O', 'X', 'O']]
+      end
+      subject(:board_place_X) { described_class.new(board) }
+
+      it 'updates space token with X token at [1, 1] only' do
+        expected_board = [['X', ' ', 'O'],
+                          [' ', 'X', 'X'],
+                          ['O', 'X', 'O']]
+        board_place_X.place_token('X', [1, 1])
+        actual_board = board_place_X.board
+        expect(actual_board).to eql(expected_board)
+      end
+    end
+
+    context 'when place O token over O token at [2, 0]' do
+      let(:board) do
+        [['X', ' ', 'O'],
+         [' ', ' ', 'X'],
+         ['O', 'X', 'O']]
+      end
+      subject(:board_place_same) { described_class.new(board) }
+
+      it 'keeps the board unchanged' do
+        expected_board = [['X', ' ', 'O'],
+                          [' ', ' ', 'X'],
+                          ['O', 'X', 'O']]
+        board_place_same.place_token('O', [2, 0])
+        actual_board = board_place_same.board
+        expect(actual_board).to eql(expected_board)
+      end
+    end
+
+    context 'when place X token off the board at [-1, -1]' do
+      let(:board) do
+        [['X', ' ', 'O'],
+         [' ', ' ', 'X'],
+         ['O', 'X', 'O']]
+      end
+      subject(:board_place_negative) { described_class.new(board) }
+
+      it 'keeps the board unchanged' do
+        expected_board = [['X', ' ', 'O'],
+                          [' ', ' ', 'X'],
+                          ['O', 'X', 'O']]
+        board_place_negative.place_token('X', [-1, -1])
+        actual_board = board_place_negative.board
+        expect(actual_board).to eql(expected_board)
+      end
+    end
+
+    context 'when place O token off the board at [3, 3]' do
+      let(:board) do
+        [['X', ' ', 'O'],
+         [' ', ' ', 'X'],
+         ['O', 'X', 'O']]
+      end
+      subject(:board_place_over_two) { described_class.new(board) }
+
+      it 'keeps the board unchanged' do
+        expected_board = [['X', ' ', 'O'],
+                          [' ', ' ', 'X'],
+                          ['O', 'X', 'O']]
+        board_place_over_two.place_token('O', [3, 3])
+        actual_board = board_place_over_two.board
+        expect(actual_board).to eql(expected_board)
+      end
+    end
+
+    context 'when place X token off the board at [0, -10]' do
+      let(:board) do
+        [['X', ' ', 'O'],
+         [' ', ' ', 'X'],
+         ['O', 'X', 'O']]
+      end
+      subject(:board_place_mix) { described_class.new(board) }
+
+      it 'keeps the board unchanged' do
+        expected_board = [['X', ' ', 'O'],
+                          [' ', ' ', 'X'],
+                          ['O', 'X', 'O']]
+        board_place_mix.place_token('X', [0, -10])
+        actual_board = board_place_mix.board
+        expect(actual_board).to eql(expected_board)
       end
     end
   end
