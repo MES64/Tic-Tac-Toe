b7fe1b213c1bf24e7ab879f200f94502960787a5
Mark Skelton <markskelton10@gmail.com>
2025-05-02 07:15:50 +0100

    test: create #check_game_over tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -85,4 +85,86 @@ RSpec.describe Game do
       game_make_move.make_move(player)
     end
   end
+
+  describe '#check_game_over' do
+    # Located in Public Script Method #play_turn
+    # Incoming Command Message -> Test the change in the observable state
+    # 4 Possibilities:
+    # 1: No winner and board not full -> result = nil
+    # 2: Winner and board not full -> result = winner_string
+    # 3: No winner and board full -> result = draw_string
+    # 4: Winner and board full -> result = winner_string
+
+    let(:board) { instance_double(Board) }
+    let(:player) { instance_double(Player) }
+
+    before do
+      allow(board).to receive(:board)
+      allow(player).to receive(:token).and_return('X')
+    end
+
+    context 'when there is no winner and the board is not full' do
+      subject(:game_result_nil) { described_class.new(board:, players: [player, player]) }
+
+      before do
+        allow(game_result_nil).to receive(:board).and_return(board)
+        allow(player).to receive(:winner?).and_return(false)
+        allow(board).to receive(:full?).and_return(false)
+      end
+
+      it 'leaves the result as nil' do
+        game_result_nil.check_game_over(player)
+        result = game_result_nil.result
+        expect(result).to be_nil
+      end
+    end
+
+    context 'when there is a winner and the board is not full' do
+      subject(:game_result_winner) { described_class.new(board:, players: [player, player]) }
+
+      before do
+        allow(game_result_winner).to receive(:board).and_return(board)
+        allow(player).to receive(:winner?).and_return(true)
+        allow(board).to receive(:full?).and_return(false)
+      end
+
+      it 'sets the result to the winner string' do
+        game_result_winner.check_game_over(player)
+        result = game_result_winner.result
+        expect(result).to eql('Player X has won the game!')
+      end
+    end
+
+    context 'when there is no winner and the board is full' do
+      subject(:game_result_draw) { described_class.new(board:, players: [player, player]) }
+
+      before do
+        allow(game_result_draw).to receive(:board).and_return(board)
+        allow(player).to receive(:winner?).and_return(false)
+        allow(board).to receive(:full?).and_return(true)
+      end
+
+      it 'sets the result to the draw string' do
+        game_result_draw.check_game_over(player)
+        result = game_result_draw.result
+        expect(result).to eql('Draw!')
+      end
+    end
+
+    context 'when there is a winner and the board is full' do
+      subject(:game_result_full_winner) { described_class.new(board:, players: [player, player]) }
+
+      before do
+        allow(game_result_full_winner).to receive(:board).and_return(board)
+        allow(player).to receive(:winner?).and_return(true)
+        allow(board).to receive(:full?).and_return(true)
+      end
+
+      it 'sets the result to the winner string' do
+        game_result_full_winner.check_game_over(player)
+        result = game_result_full_winner.result
+        expect(result).to eql('Player X has won the game!')
+      end
+    end
+  end
 end
